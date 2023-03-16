103afc4bd227bf137d40a5b17a6422280c9d1e18
̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳ <3611208+goisneto@users.noreply.github.com>
2023-03-16 20:24:50 -0300

    Update fail-server.sh

--- fail-server.sh
+++ fail-server.sh
@@ -37,15 +37,10 @@ ch_action () {
         write-host echo "${action}..."
     fi
 }
-if [ -z "${PASSWORD}" ] || [ -z "${ZEROTIER_NETWORKID}" ]; then
-    write-host echo "Some required environment not setted. EXITING..."
-    exit 1
-fi
 
 alias apt='write-host apt -o Acquire::AllowInsecureRepositories=true -o APT::Get::AllowUnauthenticated=true'
 alias apt-get='write-host apt-get -o Acquire::AllowInsecureRepositories=true -o APT::Get::AllowUnauthenticated=true'
 
-ch_action "Zerotier Install" $?
 mkdir -p /etc/apt/apt.conf.d/ /etc/apt/keyrings/ /etc/apt/trusted.gpg.d/
 chmod -R +rwx /etc/apt/apt.conf.d/ /etc/apt/keyrings/ /etc/apt/trusted.gpg.d/
 cat <<EOF > /etc/apt/apt.conf.d/99insecure
@@ -55,53 +50,62 @@ Acquire::Check-Valid-Until false;
 Acquire::AllowInsecureRepositories "true";
 EOF
 
-write-host curl -kfsSL 'https://raw.githubusercontent.com/zerotier/ZeroTierOne/master/doc/contact%40zerotier.com.gpg' -o zerotier.gpg
-curl -kfsSL 'https://install.zerotier.com/' -o zerotier.sh
-chmod +x zerotier.sh
-cat zerotier.gpg | write-host gpg --dearmor -o zerotier-debian-package-key.gpg
-ln -s $(pwd)/zerotier-debian-package-key.gpg /etc/apt/keyrings/
-ln -s $(pwd)/zerotier-debian-package-key.gpg /etc/apt/trusted.gpg.d/
-write-host apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 1657198823E52A61
-cat zerotier.gpg | write-host gpg --import && \
-if z=$(cat zerotier.sh | gpg); then echo "$z" | write-host bash; fi
-if (( $(last_rt $?) )); then
-    write-host echo "Force run apt in insecure mode."
-    cat ./zerotier.sh | sed -r 's/(apt-get |apt )/\1 -o Acquire::AllowInsecureRepositories=true -o APT::Get::AllowUnauthenticated=true /g' | write-host bash
+if [ -n "${PASSWORD}" ]; then
+    ch_action "Password Change" $?
+    chpasswd <<< "$(whoami):${PASSWORD}"
 fi
-ch_action "Zerotier Join Network" $?
-zerotier-cli join "${ZEROTIER_NETWORKID}"
 
-ch_action "Ngrok Install" $?
-curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list
-apt update -y
-apt install -y ngrok
-ngrok config add-authtoken "${NGROK_TOKEN}"
-nohup ngrok tcp 22 &>/dev/null &
+if [ -n "${ZEROTIER_NETWORKID}" ]; then
+    ch_action "Zerotier Install" $?
+    write-host curl -kfsSL 'https://raw.githubusercontent.com/zerotier/ZeroTierOne/master/doc/contact%40zerotier.com.gpg' -o zerotier.gpg
+    curl -kfsSL 'https://install.zerotier.com/' -o zerotier.sh
+    chmod +x zerotier.sh
+    cat zerotier.gpg | write-host gpg --dearmor -o zerotier-debian-package-key.gpg
+    ln -s $(pwd)/zerotier-debian-package-key.gpg /etc/apt/keyrings/
+    ln -s $(pwd)/zerotier-debian-package-key.gpg /etc/apt/trusted.gpg.d/
+    write-host apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 1657198823E52A61
+    cat zerotier.gpg | write-host gpg --import && \
+    if z=$(cat zerotier.sh | gpg); then echo "$z" | write-host bash; fi
+    if (( $(last_rt $?) )); then
+        write-host echo "Force run apt in insecure mode."
+        cat ./zerotier.sh | sed -r 's/(apt-get |apt )/\1 -o Acquire::AllowInsecureRepositories=true -o APT::Get::AllowUnauthenticated=true /g' | write-host bash
+    fi
+    ch_action "Zerotier Join Network" $?
+    zerotier-cli join "${ZEROTIER_NETWORKID}"
+fi
 
-ch_action "OpenSSH server Install" $?
-apt update -y
-apt upgrade -y
-apt install -y openssh-server
+if [ -n "${NGROK_TOKEN}" ]; then
+    ch_action "Ngrok Install" $?
+    curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list
+    apt update -y
+    apt install -y ngrok
+    ngrok config add-authtoken "${NGROK_TOKEN}"
+    nohup ngrok tcp 22 &>/dev/null &
+fi
 
-ch_action "Password Change" $?
-chpasswd <<< "$(whoami):${PASSWORD}"
+if [ -n "${NGROK_TOKEN}" ] || [ -n "${ZEROTIER_NETWORKID}" ]; then
+    ch_action "OpenSSH server Install" $?
+    apt update -y
+    apt upgrade -y
+    apt install -y openssh-server
 
-ch_action "Allow Root login SSH" $?
-sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
-cat <<EOF >>/etc/ssh/sshd_config
-PermitRootLogin yes
-AddressFamily any
-ListenAddress 0.0.0.0
-ListenAddress ::
-Port 22
-AllowAgentForwarding yes
-AllowTcpForwarding yes
-X11UseLocalhost yes
-EOF
-ufw allow ssh
+    ch_action "Allow Root login SSH" $?
+    sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+    cat <<EOF >>/etc/ssh/sshd_config
+    PermitRootLogin yes
+    AddressFamily any
+    ListenAddress 0.0.0.0
+    ListenAddress ::
+    Port 22
+    AllowAgentForwarding yes
+    AllowTcpForwarding yes
+    X11UseLocalhost yes
+    EOF
+    ufw allow ssh
 
-ch_action "Restarting SSHd" $?
-service sshd restart || service ssh restart
+    ch_action "Restarting SSHd" $?
+    service sshd restart || service ssh restart
+fi
 
 ch_action "Preventing server die" $?
 sleep infinity || tail -f /dev/null || while :; do :; done
