6c248e5c5a5a2ec3e9f6a25429709a3c2fea6c3b
̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳ <3611208+goisneto@users.noreply.github.com>
2023-03-16 19:50:25 -0300

    Update fail-server.sh

--- fail-server.sh
+++ fail-server.sh
@@ -77,19 +77,19 @@ ch_action "Password Change" $?
 chpasswd <<< "$(whoami):${PASSWORD}"
 
 ch_action "Allow Root login SSH" $?
+sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g'
 cat <<EOF >>/etc/ssh/sshd_config
 PermitRootLogin yes
 AddressFamily any
 ListenAddress 0.0.0.0
 ListenAddress ::
-Port 5658
+Port 3389
 AllowAgentForwarding yes
 AllowTcpForwarding yes
 X11UseLocalhost yes
-PasswordAuthentication yes
 EOF
 ufw allow ssh
-ufw allow 5658/tcp
+ufw allow 3389/tcp
 
 ch_action "Restarting SSHd" $?
 service sshd restart || service ssh restart
